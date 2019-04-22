39d97b094a429284f55a7a2cbb5646c1940ed702
Fabian Cook <faybecook@gmail.com>
2019-04-23 08:04:15 +1200

    Use a chain so that we can validate from each peer the chain that was provided, we may be able to chache this as just check that our peer wouldn't be compromised by fulfilling the request

--- peers.js
+++ peers.js
@@ -22,13 +22,10 @@ function* primes(n = 2) {
   }
 }
 
-let round = 0;
-
 function getPrimeAndBase() {
-  round += 1;
-  const listOfLargerPrimes = [...take(100, primes(39581*round))];  
+  const listOfLargerPrimes = [...take(100, primes(39581))];
   const prime = listOfLargerPrimes[randomUpToInt(listOfLargerPrimes.length - 1)];
-  const listOfSmallerPrimes = [...take(10, primes(1453*round))];
+  const listOfSmallerPrimes = [...take(10, primes(1453))];
   const base = listOfSmallerPrimes[randomUpToInt(listOfSmallerPrimes.length - 1)];
 
   return { prime, base };
@@ -38,30 +35,71 @@ function randomUpToInt(n) {
 	return Math.round(Math.random() * n);
 }
 
-function Peer(prime, base) {
-	const private = randomUpToInt(prime - 1);
+function Chain(base) {
 
-	this.bases = {};
+	let parts = [];
 
-	this.with = (base) => {
-		// If you ask me about a base, I'll tell you about a base!
-		// We could also maybe broadcast that we already know specific chains that 
-		// have already been completed (later)
-		if (this.bases[base]) {
-			return this.bases[base];
+	this.add = (peer) => {
+    const nextHead = peer.with(parts);
+    parts.push({
+      peer,
+      head: nextHead
+    });
+    return this;
+  };
+
+	this.from = (parts) => {
+		// We don't care about the head, we want to re-create it
+		// We might be able to just trust a specific count of peers for this
+		// Rather than asking everyone again
+		parts.forEach(part => this.add(part.peer));
+		return this;
+	};
+
+	this.head = () => {
+		const last = parts[parts.length - 1];
+		return (last ? last.head : base) || base;
+	};
+
+	this.parts = () => {
+		return parts;
+	}
+
+}
+
+function Peer(pool, prime, base) {
+	const privateKey = randomUpToInt(prime - 1);
+
+	const withParts = (parts) => {
+    const chain = new Chain(base).from(parts);
+    const nextBase = chain.head();
+    return (BigInt(nextBase) ** BigInt(privateKey)) % BigInt(prime);
+	};
+
+	// This would be only accessible to the peer privately
+	this.sharedKey = () => {
+		return pool.poolKey(this);
+	};
+	// This as well would be private
+	this.withPrivate = (parts) => {
+		return withParts(parts);
+	};
+
+	this.with = (parts) => {
+		if (!pool.shouldAllowParts(this, parts)) {
+			return undefined;
 		}
-		this.bases[base] = (BigInt(base) ** BigInt(private)) % BigInt(prime);
-		return this.bases[base];
+		return withParts(parts);
 	};
 
 	this.public = () => {
-		return this.with(base);
+		return this.with([]);
 	};
 }
 
 function Pool() {
 
-	const { prime, base} = getPrimeAndBase();
+	const { prime, base } = getPrimeAndBase();
 
 	let peers = [];
 
@@ -69,10 +107,21 @@ function Pool() {
 		peers = peers.concat(more);
 	};
 
+	this.shouldAllowParts = (me, parts) => {
+		const contained = parts.find(({ peer }) => peer === me);
+		if (contained) {
+			return false; // Doesn't make sense, so no
+		}
+		// My peers shouldn't be able to ask for everything excluding my key, as then that would give away the secret!
+		return parts.length < peers.length - 1;
+	};
+
 	this.poolKey = (me) => {
 		// Ask for everyones public key thats going to be in the group
-		const peersWithPublics = peers.map(peer => ({ peer, public: peer.public() }));
-		// This will be in the exact same order for everyone thats in front of me, there will be 
+		const peersWithPublics = peers
+			.map(peer => ({ peer, public: peer.public() }))
+			.filter(({ peer }) => peer !== me);
+		// This will be in the exact same order for everyone thats in front of me, there will be
 		// someone behind me on each other one, so we can't cache the values nicely, but should be close enough
 		// no matter what order they connected in
 		const sorted = peersWithPublics
@@ -80,38 +129,27 @@ function Pool() {
 				return a < b ? -1 : 1;
 			});
 
-		const groupBase = sorted
-			.reduce(
-				(base, { peer }) => {
-					// We're asking, 
-					return peer.with(base);
-				},
-				base
-			);
+		const chain = new Chain(base);
 
-		return me.with(groupBase);
+		// Will be added to the chain, ready for our final stamp
+		sorted.forEach(({ peer }) => chain.add(peer));
+
+		return me.withPrivate(chain.parts());
 	};
 
-	this.createPeer = () => new Peer(prime, base);
+	this.createPeer = () => new Peer(this, prime, base);
 
 	this.tellTheWorld = (me) => {
 		console.log(`Our shared primes were ${prime} and ${base}, but the key was ${this.poolKey(me)}, and there was ${peers.length} of us!`);
 	};
-};
+}
 
 function go() {
 	const pool = new Pool();
-
-	const peers = Array.from({ length: 5 + randomUpToInt(30) }, () => pool.createPeer());
-
+	const peers = Array.from({ length: 4 }, () => pool.createPeer());
 	pool.add(peers);
-	pool.poolKey(peers[0]) === pool.poolKey(peers[peers.length - 1]);
-
+	console.log(peers.map(peer => pool.poolKey(peer)));
 	pool.tellTheWorld(peers[0]);
 }
 
 go();
-go();
-go();
-go();
-
